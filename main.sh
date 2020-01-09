990f350a4d146b8fe73861ecdc9b3de6b3b93fc4
danielshow <danielshotonwa53@gmail.com>
2020-01-09 20:52:10 +0100

    set up basic stuff

--- main.sh
+++ main.sh
@@ -31,25 +31,15 @@ function open_workspace(){
   done
 }
 
-#if [ -z "$TMUX"  ]; then
-base_session='my_work_space'
-# Create a new session if it doesn't exist
-tmux has-session -t $base_session || tmux new-session -d -s $base_session
-# Are there any clients connected already?
-client_cnt=$(tmux list-clients | wc -l)
-session_name=$base_session
-if [ $client_cnt -ge 1  ]; then
-  tmux new-session -d -t $base_session
-  tmux attach-session -t $session_name
+function set_requirement(){
+  base_session='my_work_space'
+  session_name=$base_session
+  tmux new-session -d -s $base_session
+  tmux -2 attach-session -t $session_name >/dev/null 2>&1
   open_workspace $base_session
   tmux new-window -t $session_name:2
   tmux send-keys -t $session_name:2 fluxx Enter
   tmux send-keys -t $session_name:2 'vim .' Enter
-else
-  tmux -2 attach-session -t $base_session
-  open_workspace $base_session
-  tmux new-window -t $base_session:2
-  tmux send-keys -t $base_session:2 fluxx Enter
-  tmux send-keys -t $base_session:2 'vim .' Enter
-fi
-#fi
+}
+
+set_requirement
