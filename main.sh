f655783f9621af87e408230404545580880228af
danielshow <danielshotonwa53@gmail.com>
2020-01-09 17:52:59 +0100

    initial setup working

--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "yes"
 function open_workspace(){
-  echo are you the one
   for f in `seq 1 2`; do
     tmux split-window -h -t $1
     tmux send-keys -t $1:1 fluxx Enter
@@ -10,25 +8,25 @@ function open_workspace(){
 }
 
 #if [ -z "$TMUX"  ]; then
-echo what is wrong
 base_session='my_work_space'
 # Create a new session if it doesn't exist
 tmux has-session -t $base_session || tmux new-session -d -s $base_session
 # Are there any clients connected already?
 client_cnt=$(tmux list-clients | wc -l)
+session_name=$base_session
 if [ $client_cnt -ge 1  ]; then
-  echo i got here again
-  session_name=$base_session"-"$client_cnt
-  tmux new-session -d -t $base_session -s $session_name
-  echo i got here again
-  tmux -2 attach-session -t $session_name
+  echo "this is me"
+  tmux new-session -d -t $base_session
+  tmux attach-session -t $session_name
   open_workspace $base_session
   tmux new-window -t $session_name:2
   tmux send-keys -t $session_name:2 vim Enter
 else
-  echo I stooped here
   tmux -2 attach-session -t $base_session
   open_workspace $base_session
+  tmux new-window -t $base_session:2
+  tmux send-keys -t $base_session:2 fluxx Enter
+  #tmux send-keys -t $base_session:2 vim Enter
 fi
 #fi
 
